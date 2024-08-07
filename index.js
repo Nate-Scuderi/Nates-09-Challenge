f279661069d7128976a4ac02baba7004071b2870
Nate-Scuderi <159470103+Nate-Scuderi@users.noreply.github.com>
2024-08-06 20:31:44 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,145 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Include packages needed for this application
+// Inquirer is already included in package.json
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+  return `[License](https://opensource.org/licenses/${license})`;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return `## License
+
+This project is licensed under the ${license} license. ${renderLicenseLink(license)}
+  `;
+}
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv3', 'Apache_2.0', 'BSD_3--Clause', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('README.md file has been generated');
+  });
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions about the project, you can reach me at [${data.email}](mailto:${data.email}).  
+GitHub: [${data.github}](https://github.com/${data.github})
+
+${renderLicenseSection(data.license)}
+`;
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdown = generateMarkdown(answers);
+    writeToFile('README.md', markdown);
+  });
+}
+
+// Function call to initialize app
+init();
+
+module.exports = generateMarkdown;
